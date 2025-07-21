b4881f63422e7515bff92747239624f64b257925
psamborski <psambek@gmail.com>
2025-07-22 00:12:13 +0200

    Console log clean up. Error screen test.

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,16 +4,12 @@ import {CONFIG} from '/src/utils/config'
 
 export function createAxiosInstance({
                                       formData = false,
-                                      authorization = `Bearer ${CONFIG.CONTENTFUL_CONSUME_API_KEY || import.meta.env.VITE_CONTENTFUL_CONSUME_API_KEY}`,
+                                      authorization = `Bearer ${CONFIG.CONTENTFUL_CONSUME_API_KEY || import.meta.env.ITE_CONTENTFUL_CONSUME_API_KEY}`,
                                       baseUrl = `https://graphql.contentful.com/content/v1/spaces/${CONFIG.CONTENTFUL_SPACE_ID}/`
                                     } = {}) {
   const instance = axios.create({
     baseURL: baseUrl,
   })
-
-  console.log(CONFIG.CONTENTFUL_CONSUME_API_KEY)
-  console.log(import.meta.env)
-  console.log(CONFIG.CONTENTFUL_CONSUME_API_KEY || import.meta.env.VITE_CONTENTFUL_CONSUME_API_KEY)
 
   instance.defaults.headers.common['Authorization'] = authorization
 

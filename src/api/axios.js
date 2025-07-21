4a032457488f16b05bdec05b8e41f36006804f27
psamborski <psambek@gmail.com>
2025-07-22 00:14:25 +0200

    Error screen ok, clean version push.

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import {CONFIG} from '/src/utils/config'
 
 export function createAxiosInstance({
                                       formData = false,
-                                      authorization = `Bearer ${CONFIG.CONTENTFUL_CONSUME_API_KEY || import.meta.env.ITE_CONTENTFUL_CONSUME_API_KEY}`,
+                                      authorization = `Bearer ${CONFIG.CONTENTFUL_CONSUME_API_KEY || import.meta.env.VITE_CONTENTFUL_CONSUME_API_KEY}`,
                                       baseUrl = `https://graphql.contentful.com/content/v1/spaces/${CONFIG.CONTENTFUL_SPACE_ID}/`
                                     } = {}) {
   const instance = axios.create({
